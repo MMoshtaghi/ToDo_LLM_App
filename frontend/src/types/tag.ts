9ec6a6df607d2f05a22b608d014ddb48d9a882f5
Mehdi Moshtaghi <moshtaghi.uni@gmail.com>
2025-05-23 16:40:27 +0300

    Added task and tag types

---
+++ frontend/src/types/tag.ts
@@ -0,0 +1,17 @@
+import type { TaskResponse } from "./task";
+
+export interface TagBase {
+  tag: string;
+}
+
+export type TagCreate = TagBase
+
+export interface TagResponse extends TagBase {
+  id: number;
+}
+
+export interface TagResponseWithTasks extends TagResponse {
+  tasks: TaskResponse[];
+}
+
+export type TagUpdate = TagBase
---
+++ frontend/src/types/task.ts
@@ -0,0 +1,28 @@
+import type { TagResponse } from "./tag";
+
+export interface TaskCreate {
+  title: string;
+  description?: string | null;
+  is_done: boolean;
+  scheduled_for?: string | null;
+}
+
+export interface TaskUpdate {
+  title?: string | null;
+  description?: string | null;
+  is_done?: boolean | null;
+  scheduled_for?: string | null;
+}
+
+export interface TaskResponse {
+  id: number;
+  title: string;
+  description?: string | null;
+  is_done: boolean;
+  scheduled_for?: string | null;
+  created_at: string;
+}
+
+export interface TaskResponseWithTags extends TaskResponse {
+  tags: TagResponse[];
+}
